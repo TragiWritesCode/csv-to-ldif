e63edf9ec495d007844bf3c88e1043b44ba8fccc
Stefan Trageiser <spam.stefan@gmx.de>
2026-02-11 22:14:16 +0100

    new logic

--- main.go
+++ main.go
@@ -2,17 +2,42 @@ package main
 
 import (
 	"bufio"
-	"log"
+	"fmt"
 	"os"
 	"strings"
 )
 
-func checkFile(e error) {
+func check(e error) {
 	if e != nil {
-		log.Fatal(e)
+		panic(e)
 	}
 }
 
 func main() {
-}
+	csvFile, err := os.Open("test.csv")
+	check(err)
+	scanner := bufio.NewScanner(csvFile)
+	scanner.Scan()
 
+	//https://www.bytesizego.com/blog/reading-file-line-by-line-golang
+	keys := strings.Split(scanner.Text(), ";")
+	fmt.Println(keys)
+
+	var totalVals [][]string
+	for scanner.Scan() {
+		vals := strings.Split(scanner.Text(), ";")
+		totalVals = append(totalVals, vals)
+		totalVals = append(totalVals, vals)
+	}
+	fmt.Println(keys)
+	fmt.Println(totalVals)
+	var out string
+	for i := 0; i < len(keys); i++ {
+		out += keys[i] + ": " + totalVals[i][i] + "\n"
+	}
+
+	fmt.Println(out)
+
+	csvFile.Close()
+
+}

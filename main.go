1483119940e2ea752664652b0f4e5497de29a880
Stefan Trageiser <spam.stefan@gmx.de>
2026-02-11 20:59:35 +0100

    removed previous logic

--- main.go
+++ main.go
@@ -14,50 +14,5 @@ func checkFile(e error) {
 }
 
 func main() {
-	file, err := os.Open("test.csv")
-	checkFile(err)
-
-	fileScanner := bufio.NewScanner(file)
-	fileScanner.Split(bufio.ScanLines)
-
-	out, err := os.Create("out.ldif")
-	checkFile(err)
-
-	outStr := ""
-	dn := "dn: "
-	var restOfLines []string
-	fileScanner.Scan()
-	firstLine := fileScanner.Text()
-
-	for fileScanner.Scan() {
-		restOfLines = append(restOfLines, fileScanner.Text())
-	}
-
-	defer file.Close()
-
-	firstLineValues := strings.Split(firstLine, ";")
-
-	for _, line := range restOfLines {
-		splitValues := strings.Split(line, ";")
-
-		for i := 0; i < len(firstLineValues); i++ {
-			switch firstLineValues[i] {
-			case "uid":
-				dn += firstLineValues[i] + "=" + splitValues[i]
-			case "ou":
-				dn += firstLineValues[i] + "=" + splitValues[i]
-			case "dc":
-				dn += firstLineValues[i] + "=" + splitValues[i]
-			}
-			if splitValues[i] != "" {
-				outStr += firstLineValues[i] + ": " + splitValues[i] + "\n"
-			}
-		}
-		outStr += dn
-		dn = "dn: "
-	}
-
-	out.WriteString(outStr)
-
-	defer out.Close()
 }
+
